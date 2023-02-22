1159cc9aaae0135cc9a14ec4901ca99ac67f51f7
sirisha517 <sai.sirifriends8790@gmail.com>
2023-02-22 16:33:16 +0530

    updating learn-shell

--- 03-var.sh
+++ 03-var.sh
@@ -3,3 +3,6 @@ echo "Hello GOOD MORNING ,Today DATE IS ${DATE}"
 # command substitution
 DATE=$(date)
 echo "Hello GOOD MORNING ,Today DATE IS ${DATE}"
+# Using Arithematic Subtitution
+ADD=$((2+3))
+echo "Addition of 2 + 3 = ${ADD}"

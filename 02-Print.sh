bdd5cff41b39c344408e7251c13cc52bd529750e
sirisha517 <sai.sirifriends8790@gmail.com>
2023-02-22 16:11:22 +0530

    updating learn-shell

--- 02-Print.sh
+++ 02-Print.sh
@@ -3,17 +3,17 @@ echo Hello World
 
 ## colour syntax
 
-# **echo -e “\e[COLm MESSAGE  \e[0m”**
+# echo -e “\e[COLm MESSAGE  \e[0m”
 # -e —> enable colour from the echo command
 # \e[COLm  —> Enable a particular colour
 # \e[0m  —> zero code is reset the colour, meaning if we enable colour we should disable it
 # **colour             codes**
 # Red                  31
-# Green               32
+# Green                32
 # Yellow               33
-# Blue                  34
-# Magenta            35
+# Blue                 34
+# Magenta              35
 # Cyan                 36
-echo -e “\e[31mHello World in red colour\e[0m”
+echo -e “\e[31m Hello World in red colour \e[0m”
 echo Hello World
-echo -e “\e[35mSirisha Kallempudi in red colour\e[0m”
+echo -e “\e[35m Sirisha Kallempudi in red colour \e[0m”

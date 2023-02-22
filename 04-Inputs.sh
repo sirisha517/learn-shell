b8caee74753836e13bb1e87214e216c36e7eb1a5
sirisha517 <sai.sirifriends8790@gmail.com>
2023-02-22 16:43:22 +0530

    updating learn-shell

--- 04-Inputs.sh
+++ 04-Inputs.sh
@@ -1,2 +1,11 @@
-echo $1
-echo $2
+# 0-n , $*/$@, $#
+# These are the special variables ,we cannot declare
+echo First Argument = $1 # First Argument
+echo Second Argument =  $2 # Second Argument
+
+echo Script Name = $0 # Script Name
+
+echo All Arguments = $*
+echo All Arguments = $@ # All Arguments
+
+echo NUmber of Arguments = $# # NUmber of Arguments

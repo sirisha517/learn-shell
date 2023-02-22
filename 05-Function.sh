9f88e843c02b453151a9c6bfb09ec72f064c289d
sirisha517 <sai.sirifriends8790@gmail.com>
2023-02-22 18:22:55 +0530

    updating learn-shell

--- 05-Function.sh
+++ 05-Function.sh
@@ -2,6 +2,12 @@
 
 xyz() {
   echo Hello from Function
+  echo First Argument - $1
+  echo Second Argument -- $2
+  echo All Argument - $*
+  echo No of Argument - $#
+
 }
+## Main program
 ## call a Function
-xyz
+xyz 123 456

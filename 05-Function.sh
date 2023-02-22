539660cb5dacb1e1736e4ac45cc785a8928dc459
sirisha517 <sai.sirifriends8790@gmail.com>
2023-02-22 18:26:08 +0530

    updating learn-shell

--- 05-Function.sh
+++ 05-Function.sh
@@ -6,8 +6,12 @@ xyz() {
   echo Second Argument -- $2
   echo All Argument - $*
   echo No of Argument - $#
+  echo value of a = $a
+  b=200
 
 }
 ## Main program
 ## call a Function
+a=120
 xyz 123 456
+echo value of b is $b

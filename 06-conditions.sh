cbbb6d64ee1cf4642b3ef75ce9a88a3236871ee3
sirisha517 <sai.sirifriends8790@gmail.com>
2023-02-23 16:14:18 +0530

    updating learn-shell

--- 06-conditions.sh
+++ 06-conditions.sh
@@ -1,11 +1,11 @@
 number=$1
 string=$2
 
-if [ "$(number)" -eq 5 ]; then #then can go in next line as well, also semi colon is also fine in the same line
+if [ "${number}" -eq 5 ]; then #then can go in next line as well, also semi colon is also fine in the same line
  echo number is 5
 fi
 
-if [ "$(string)" == abc ]; then
+if [ "${string}" == abc ]; then
  echo string is sirisha
 else
   echo string is not sirisha
